c00e87c87dc8a59feea3940d201155fae19ea6ec
Dillon Schultz <dillonschultz93@gmail.com>
2022-04-06 10:15:49 -0700

    Clean up scripts directory

--- docs/assets/js/utilities/pasta-doc-utilities.js
+++ docs/assets/js/utilities/pasta-doc-utilities.js
@@ -181,3 +181,15 @@ function handleCopyTokensToClipboard(buttonElement, tokens) {
     handleCopyToClipboard(tokens);
   }
 }
+
+// TOOL CLIP HANDLER
+
+const allToolClipButtons = [...document.querySelectorAll('span[data-toolclip]')];
+
+if (allToolClipButtons.length > 0) {
+  // Set up all toolclip buttons
+  allToolClipButtons.forEach(button => {
+    const data = button.dataset.toolclip;
+    button.addEventListener('click', () => handleCopyToClipboard(data));
+  });
+}
