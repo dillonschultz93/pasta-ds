815e36875d09c5d28da87efb88605bfb79652292
Dillon Schultz <dillonschultz93@gmail.com>
2022-09-13 16:37:03 -0700

    Automate header with tokens

--- postinstall.js
+++ postinstall.js
@@ -1,20 +1,17 @@
-const { writeFileSync } = require('fs')
-const { getByPage, getFlattened, getValueFromFlatTokens } = require('@yummly/pasta-dictionary')
-
-const tokens = getFlattened(); //?
-
-getValueFromFlatTokens(getByPage(tokens, 'E0003')) //?
+const { writeFileSync } = require('fs');
+const { getByPage, getTokens, getValueFromFlatTokens, getNestedTokens } = require('@yummly/pasta-dictionary/src/index');
 
 const uids = ['E0003'];
 
 uids.forEach(uid => {
-    const tokens = getByPage(getFlattened(), uid);
-    const values = Object.entries(
-        getValueFromFlatTokens(tokens)
-    ).map(([key, value]) => ({[key.split('-').slice(1).join('-')]: value}))
-    .reduce((acc, curr) => ({...acc, ...curr}), {});
+	const tokens = getByPage(getTokens(), uid);
+  const values = getValueFromFlatTokens(tokens);
 
-    console.log(values)
-    
-    writeFileSync(`./docs/_data/${uid}.json`, JSON.stringify(values, null, 2));
-})
+	const meta = Object.entries(tokens)
+		.filter(([key]) => key.startsWith('meta'))
+		.reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+  const nestedMeta = getNestedTokens(meta, true);
+
+	writeFileSync(`./docs/_data/${uid}.json`, JSON.stringify({ tokens: values, meta: nestedMeta.meta[uid] }, null, 2));
+});
