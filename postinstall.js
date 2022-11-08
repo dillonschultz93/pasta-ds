3aa05cc5234611191dbe8c8cdcf013146340a840
Dillon Schultz <dillonschultz93@gmail.com>
2022-11-07 18:02:21 -0700

    Comment out postinstall script as we don't need it with the new tool

--- postinstall.js
+++ postinstall.js
@@ -1,17 +1,17 @@
-const { writeFileSync } = require('fs');
-const { getByPage, getTokens, getValueFromFlatTokens, getNestedTokens } = require('@yummly/pasta-dictionary/src/index');
+// const { writeFileSync } = require('fs');
+// const { getByPage, getTokens, getValueFromFlatTokens, getNestedTokens } = require('@yummly/pasta-dictionary/src/index');
 
-const uids = ['E0003'];
+// const uids = ['E0003'];
 
-uids.forEach(uid => {
-	const tokens = getByPage(getTokens(), uid);
-  const values = getValueFromFlatTokens(tokens);
+// uids.forEach(uid => {
+// 	const tokens = getByPage(getTokens(), uid);
+//   const values = getValueFromFlatTokens(tokens);
 
-	const meta = Object.entries(tokens)
-		.filter(([key]) => key.startsWith('meta'))
-		.reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+// 	const meta = Object.entries(tokens)
+// 		.filter(([key]) => key.startsWith('meta'))
+// 		.reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
 
-  const nestedMeta = getNestedTokens(meta, true);
+//   const nestedMeta = getNestedTokens(meta, true);
 
-	writeFileSync(`./docs/_data/${uid}.json`, JSON.stringify({ tokens: values, meta: nestedMeta.meta[uid] }, null, 2));
-});
+// 	writeFileSync(`./docs/_data/${uid}.json`, JSON.stringify({ tokens: values, meta: nestedMeta.meta[uid] }, null, 2));
+// });
