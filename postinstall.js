0a48c104a8e9437a2fdcbaf13185308a29400f9c
Dillon Schultz <dillonschultz93@gmail.com>
2022-11-16 17:43:24 -0700

    Refactor postinstall script to write tokens to separate files

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,4 @@
-const { readdirSync, copyFileSync, mkdirSync } = require('fs');
+const { readdirSync, mkdirSync, writeFileSync } = require('fs');
 
 // Get the json files from the @yummly/pasta-dictionary package and move them to the _data directory
 const projectDirectories = readdirSync('./node_modules/@yummly/pasta-dictionary/dist');
@@ -10,6 +10,14 @@ projectDirectories.forEach((project) => {
   const tokenFiles = readdirSync(`./node_modules/@yummly/pasta-dictionary/dist/${project}/knowledge-base`);
 
   tokenFiles.forEach((tokenFile) => {
-    copyFileSync(`./node_modules/@yummly/pasta-dictionary/dist/${project}/knowledge-base/${tokenFile}`, `./docs/_data/${project}/${tokenFile}`);
+    const file = require(`./node_modules/@yummly/pasta-dictionary/dist/${project}/knowledge-base/${tokenFile}`);
+    const tokens = file._tokens;
+
+    // Loop through each token object and write it to a file
+    Object.keys(tokens).forEach((token) => {
+      const tokenObject = tokens[token];
+      writeFileSync
+      (`./docs/_data/${project}/${token}.json`, JSON.stringify(tokenObject, null, 2));
+    });
   });
 });
