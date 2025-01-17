78f54dcadc465de31be2c18cf436dff7c971f55c
Dillon Schultz <dillonschultz93@gmail.com>
2025-01-17 15:55:08 -0800

    Remove a previous reference to the dictionary library

--- postinstall.js
+++ postinstall.js
@@ -1,16 +1,16 @@
 const { readdirSync, mkdirSync, writeFileSync } = require('fs');
 
 // Get the json files from the @yummly/pasta-dictionary package and move them to the _data directory
-const projectDirectories = readdirSync('./node_modules/@yummly/pasta-dictionary/dist');
+const projectDirectories = readdirSync('./node_modules/@yosi_/ypl-dictionary/dist');
 
 projectDirectories.forEach((project) => {
 
   mkdirSync(`./docs/_data/${project}`, { recursive: true });
 
-  const tokenFiles = readdirSync(`./node_modules/@yummly/pasta-dictionary/dist/${project}/knowledge-base`);
+  const tokenFiles = readdirSync(`./node_modules/@yosi_/ypl-dictionary/dist/${project}/knowledge-base`);
 
   tokenFiles.forEach((tokenFile) => {
-    const file = require(`./node_modules/@yummly/pasta-dictionary/dist/${project}/knowledge-base/${tokenFile}`);
+    const file = require(`./node_modules/@yosi_/ypl-dictionary/dist/${project}/knowledge-base/${tokenFile}`);
     const tokens = file._tokens;
 
     // Loop through each token object and write it to a file
